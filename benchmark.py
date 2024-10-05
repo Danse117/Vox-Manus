b323355925d02a55cc965b08fd443ad77ee9fa07
Adam E <adamelsayad2002@gmail.com>
2024-10-05 15:31:24 -0400

    First model pushed/Training Notebook

---
+++ benchmark.py
@@ -0,0 +1,5 @@
+from ultralytics.utils.benchmarks import benchmark
+
+# Benchmark on GPU
+
+benchmark(model='First_Train_20')
---
+++ test.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Load trained model
+model = YOLO('models/First_Train_20.pt')
+
+# Print model info
+print(model.info())
+# Run inference on the source
+results = model(source=1, show=True, conf=0.4, save=True, stream=True)
+
+for r in results:
+    boxes = r.boxes  # Boxes object for bbox outputs
+    probs = r.probs  # Class probabilities for classification outputs
